b806e980d3ae1a68b604acd408a763231e494883
Andreas Fredriksson <afredriksson89@gmail.com>
2024-05-27 16:30:24 +0200

    Git Changes to subscibe

--- SubscriptionProvider/Migrations/20240527142232_Added Id to entity.Designer.cs
+++ SubscriptionProvider/Migrations/20240527142232_Added Id to entity.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SubscriptionProvider.Data.Contexts;
 
@@ -10,9 +11,11 @@ using SubscriptionProvider.Data.Contexts;
 namespace SubscriptionProvider.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240527142232_Added Id to entity")]
+    partial class AddedIdtoentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
